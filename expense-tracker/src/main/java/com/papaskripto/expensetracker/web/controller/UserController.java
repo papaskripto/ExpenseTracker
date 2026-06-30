69f113f9899bc7a258ca2396b23e385750ddf42b
papaskripto <bygeoffreyouma@gmail.com>
2026-06-30 20:55:31 +0300

    Update user controller

--- expense-tracker/src/main/java/com/papaskripto/expensetracker/web/controller/UserController.java
+++ expense-tracker/src/main/java/com/papaskripto/expensetracker/web/controller/UserController.java
@@ -1,36 +1,25 @@
 package com.papaskripto.expensetracker.web.controller;
 
-import com.papaskripto.expensetracker.data.entity.UserEntity;
-import com.papaskripto.expensetracker.data.repository.UserRepository;
-import com.papaskripto.expensetracker.web.model.User;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.papaskripto.expensetracker.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping ("/users")
 
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController (UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController (UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public String getUsersPage (Model model) {
-        List<UserEntity> userEntities = this.userRepository.findAll();
-        List<User> users = new ArrayList<>(userEntities.size());
-        userEntities.forEach(userEntity -> users.add (new User(userEntity.getUserId(), userEntity.getUserEmail(), userEntity.getPassword(),
-                userEntity.getUsername(), userEntity.getCreatedAt(), userEntity.getUpdatedAt())));
-        model.addAttribute("users", users);
+        model.addAttribute("users", this.userService.listUsers());
         return "users";
     }
 }
